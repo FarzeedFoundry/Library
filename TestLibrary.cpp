df4b8a7bdfd6cf64daa80df012b36e43cb067ff0
Farzeed <farzeed.urrehman@foundry.com>
2019-04-25 13:18:03 +0100

    New repo

---
+++ TestLibrary.cpp
@@ -0,0 +1,16 @@
+#include "TestLibrary.h"
+#include <stdio.h>
+#include <Math/fnMath.h>
+
+TestLibraryNS::TestLibrary::TestLibrary()
+{
+    // Empty constructor
+}
+
+void TestLibraryNS::TestLibrary::Info()
+{
+    // Generate some info
+    printf("This is some information\n");
+	printf("Value is %i\n", Foundry::Math::Abs(-10));
+}
+
